1f6bcdf87b5034000990b16fab9341976daa7621
Luan <luan.donato10@gmail.com>
2024-10-22 00:32:20 -0300

    Feign client

---
+++ hr-payroll/src/main/java/com/study/hrpayroll/feingclients/WorkerFeingClients.java
@@ -0,0 +1,17 @@
+package com.study.hrpayroll.feingclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.study.hrpayroll.entities.Worker;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeingClients {
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+}
