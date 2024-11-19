fefd7f85366a02d8248d016c26ad601899b85633
Luan Abdo Donato <luan.donato0810@gmail.com>
2024-11-19 08:45:46 -0300

    Private git repository

--- hr-configserver/src/main/java/com/configserver/HrConfigserverApplication.java
+++ hr-configserver/src/main/java/com/configserver/HrConfigserverApplication.java
@@ -1,12 +1,20 @@
 package com.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigserverApplication {
+public class HrConfigserverApplication{
+
+	@Value("${spring.cloud.config.server.git.username}")
+	private String user;
+
+	@Value("${spring.cloud.config.server.git.password}")
+	private String password;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigserverApplication.class, args);
